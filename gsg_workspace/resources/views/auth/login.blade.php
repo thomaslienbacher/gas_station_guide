d86b4af95fc885cf3788b1bbde72704d324113a2
thomaslienbacher <lienbacher.tom@gmail.com>
2018-06-14 00:03:44 +0200

    A little of CSS

--- gsg_workspace/resources/views/auth/login.blade.php
+++ gsg_workspace/resources/views/auth/login.blade.php
@@ -12,9 +12,9 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">Email Addresse</label>
+                            <label for="email" class="col-sm-2 col-form-label text-md-right">Email Addresse</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -26,9 +26,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Passwort</label>
+                            <label for="password" class="col-md-2 col-form-label text-md-right">Passwort</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -40,26 +40,28 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-10 offset-md-2">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Mich merken
                                     </label>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Anmelden
-                                </button>
-
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Passwort vergessen?
                                 </a>
                             </div>
                         </div>
+
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary wide-button">
+                                    Anmelden
+                                </button>
+                            </div>
+                        </div>
+
+
                     </form>
                 </div>
             </div>
--- gsg_workspace/resources/views/auth/register.blade.php
+++ gsg_workspace/resources/views/auth/register.blade.php
@@ -12,9 +12,9 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                            <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -26,9 +26,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Email Addresse</label>
+                            <label for="email" class="col-md-2 col-form-label text-md-right">Email Addresse</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -40,9 +40,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Passwort</label>
+                            <label for="password" class="col-md-2 col-form-label text-md-right">Passwort</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -54,16 +54,16 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Passwort bestätigen</label>
+                            <label for="password-confirm" class="col-md-2 col-form-label text-md-right">Passwort bestätigen</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary wide-button">
                                     Registrieren
                                 </button>
                             </div>
